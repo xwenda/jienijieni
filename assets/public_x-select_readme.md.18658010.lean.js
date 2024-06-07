dab5394067a25c4e23eb819915283b2c0604c831
xwenda <156357190@qq.com>
2024-06-07 14:44:40 +0800

    2024-06-07
    删除旧文件

--- assets/public_x-select_readme.md.18658010.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as l,c as s,X as a}from"./chunks/framework.d276e8ea.js";const D=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"public/x-select/readme.md"}'),t={name:"public/x-select/readme.md"},o=a("",4),n=[o];function p(r,c,i,d,_,u){return l(),s("div",null,n)}const F=e(t,[["render",p]]);export{D as __pageData,F as default};
--- x-select/dafault-list.js
+++
@@ -1,5 +0,0 @@
-export const defaultSelect = [
-  { label: 'dictName', value: 'dictValue' },
-  { label: 'enterpriseName', value: 'orgCode' },
-  { label: 'label', value: 'value' }
-];
